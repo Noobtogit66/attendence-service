caeccce9fa00ff5f3efbcfb50179cd9aaaf9de4d
Noobtogit66 <janasagnik66@gmail.com>
2025-12-20 09:32:26 +0530

    necessary methods

--- src/main/java/com/sagnik/attendanceclient/repository/AttendenceRepository.java
+++ src/main/java/com/sagnik/attendanceclient/repository/AttendenceRepository.java
@@ -3,5 +3,10 @@ package com.sagnik.attendanceclient.repository;
 import com.sagnik.attendanceclient.model.Attendence;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AttendenceRepository extends JpaRepository<Attendence, String> {
+    Optional<Attendence> findByIdAndEpochTime(String userId, long dateEpoch);
+    List<Attendence> findAllByUserId(String userId);
 }
