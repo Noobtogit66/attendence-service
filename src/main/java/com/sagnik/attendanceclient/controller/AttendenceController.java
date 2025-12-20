0baf549786efb168fcd6199fb7ba6214eb7d4bbb
Noobtogit66 <janasagnik66@gmail.com>
2025-12-20 11:07:32 +0530

    Update AttendenceController.java

--- src/main/java/com/sagnik/attendanceclient/controller/AttendenceController.java
+++ src/main/java/com/sagnik/attendanceclient/controller/AttendenceController.java
@@ -1,16 +1,26 @@
 package com.sagnik.attendanceclient.controller;
 
 
+import com.sagnik.attendanceclient.model.Attendence;
+import com.sagnik.attendanceclient.service.AttendenceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/attendence")
 public class AttendenceController {
-
-
-    // Card Swipe Endpoint
-//        Intime, outtime , card swipe
-
-
+@Autowired
+private AttendenceService service;
+@GetMapping("/getAttendence/{userId}")
+    public ResponseEntity<Attendence> getAttendence(@PathVariable String userId)
+{
+    return ResponseEntity.status(HttpStatus.ACCEPTED).body(service.getAttendence(userId));
+}
 //    Manual Attendence Entry
 
 
