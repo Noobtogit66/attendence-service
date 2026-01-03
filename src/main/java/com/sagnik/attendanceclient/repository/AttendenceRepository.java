2a64ad3a279d08ba379462532795ac26bdc5901a
Noobtogit66 <janasagnik66@gmail.com>
2026-01-03 08:07:49 +0530

    Update AttendenceRepository.java

--- src/main/java/com/sagnik/attendanceclient/repository/AttendenceRepository.java
+++ src/main/java/com/sagnik/attendanceclient/repository/AttendenceRepository.java
@@ -1,12 +1,13 @@
 package com.sagnik.attendanceclient.repository;
 
-import com.sagnik.attendanceclient.model.Attendence;
+import com.sagnik.attendanceclient.model.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface AttendenceRepository extends JpaRepository<Attendence, String> {
-    Optional<Attendence> findByIdAndEpochTime(String userId, long dateEpoch);
-    List<Attendence> findAllByUserId(String userId);
+public interface AttendenceRepository extends JpaRepository<Attendance, String> {
+
+    List<Attendance> findAllByUserId(String userId);
+    Optional<Attendance> findByUserIdAndDateEpoch(String userId, long todayEpoch);
 }
